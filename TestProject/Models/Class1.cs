3ff38d91a9d700570f28a3ec4c90bfc29d11fc5b
Adalberto Nivar <adalberto.nivar@gmail.com>
2017-01-30 15:56:01 -0600

    Prueba cambios

--- TestProject/Models/Class1.cs
+++ TestProject/Models/Class1.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace TestProject.Models
+﻿namespace TestProject.Models
 {
     public class Class1
     {
+           
     }
 }
